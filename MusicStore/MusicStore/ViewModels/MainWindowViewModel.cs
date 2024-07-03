4fcc6a09637c38c58fde5b870adae80dfc88c2fd
KlaudiaD <dadokklaudia@gmail.com>
2024-07-03 18:46:44 +0200

    Making music store

--- MusicStore/MusicStore/ViewModels/MainWindowViewModel.cs
+++ MusicStore/MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,24 @@
-﻿namespace MusicStore.ViewModels;
+﻿using System.Reactive.Linq;
+using System.Windows.Input;
+using ReactiveUI;
 
-public class MainWindowViewModel : ViewModelBase
+namespace MusicStore.ViewModels
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ICommand BuyMusicCommand { get; }
+        public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+
+        public MainWindowViewModel()
+        {
+            ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
+
+            BuyMusicCommand = ReactiveCommand.CreateFromTask(async () =>
+            {
+                var store = new MusicStoreViewModel();
+
+                var result = await ShowDialog.Handle(store);
+            });
+        }
+    }
 }
